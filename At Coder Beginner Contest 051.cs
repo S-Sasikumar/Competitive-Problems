b6be1d3507cd7a57c8e167be0df74ab6639baaa2
S-SasiKumar <sasisadha815@gmail.com>
2021-09-08 21:39:36 +0530

    contest051

---
+++ At Coder Beginner Contest 051.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atcoderbeginnercontest051
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            haiku();
+        }
+
+        public static void haiku()
+        {
+            string a = Console.ReadLine();
+            char b = ' ';
+            int i;
+            string s = " ";
+
+            for (i = 0; i < a.Length; i++)
+            {
+                if (a[i] == ',')
+                {
+                    s = a.Replace(',', b);
+
+                }
+
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+
+        }
+    }
+}
