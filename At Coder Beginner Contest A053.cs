f413d9267619dccda8ce634fa2013bb76dc2bdc3
S-SasiKumar <sasisadha815@gmail.com>
2021-09-08 21:42:53 +0530

    contest A053

---
+++ At Coder Beginner Contest A053.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atcoderbeginner53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            abcorarc();
+        }
+        public static void abcorarc()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a < 1200)
+            {
+                Console.WriteLine("ABC");
+            }
+            else
+            {
+                Console.WriteLine("ARC");
+            }
+            Console.ReadKey();
+        }
+    }
+}
