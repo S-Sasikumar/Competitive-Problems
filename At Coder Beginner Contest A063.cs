c74ec02962332a6a89c502e952b6bc2e7faeba16
S-SasiKumar <sasisadha815@gmail.com>
2021-09-08 22:03:01 +0530

    Beginner Contest A063

---
+++ At Coder Beginner Contest A063.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderBeginnerContest_A063
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            restricted();
+        }
+
+        public static void restricted()
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+
+            if (a + b < 10)
+            {
+
+                Console.WriteLine(a + b);
+
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
