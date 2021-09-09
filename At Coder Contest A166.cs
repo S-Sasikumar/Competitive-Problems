8ad385e7a85efab56e0a082778c726fa9b6941f3
S-SasiKumar <sasisadha815@gmail.com>
2021-09-09 22:12:36 +0530

    AtCoderBeginner

---
+++ At Coder Contest A166.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderBeginnerContest_A166
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ac166();
+        }
+        public static void ac166()
+        {
+            string s = Console.ReadLine();
+            if (s == "ABC")
+            {
+                Console.WriteLine("ARC");
+            }
+            else
+            {
+                Console.WriteLine("ABC");
+            }
+            Console.ReadKey();
+        }
+    }
+}
