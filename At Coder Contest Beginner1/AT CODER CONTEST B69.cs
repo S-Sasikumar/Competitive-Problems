ece0bf2b06f55b387838960e7c69fbe827503c41
S-SasiKumar <sasisadha815@gmail.com>
2021-09-15 22:16:07 +0530

    CONTEST B69

---
+++ At Coder Contest Beginner1/AT CODER CONTEST B69.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace i18n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AT CODER CONTEST BEGINNER B 69//
+
+            string s = Console.ReadLine();
+
+            int count = 0;
+            for (int i = 1; i < s.Length - 1; i++)
+            {
+                count++;
+            }
+            Console.WriteLine(s[0] + "" + count + "" + s[s.Length - 1]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
