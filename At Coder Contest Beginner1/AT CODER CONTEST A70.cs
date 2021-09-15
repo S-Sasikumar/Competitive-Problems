7d47c3717dc61890111537331b32a48473ed5db3
S-SasiKumar <sasisadha815@gmail.com>
2021-09-15 22:19:15 +0530

    CONTEST A70

---
+++ At Coder Contest Beginner1/AT CODER CONTEST A70.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromic_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           //AT CODER BEGINNER CONTEST A 70//
+
+            string n = Console.ReadLine();
+            
+            if (n[0] == n[2])
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            Console.ReadKey();
+        }
+    }
+}
