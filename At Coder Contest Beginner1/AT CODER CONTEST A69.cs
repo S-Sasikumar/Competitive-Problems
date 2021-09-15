d233453dcd792b4f00ea6302c22b91518b9019e8
S-SasiKumar <sasisadha815@gmail.com>
2021-09-15 22:17:57 +0530

    CONTEST A69

---
+++ At Coder Contest Beginner1/AT CODER CONTEST A69.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kcity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AT CODER CONTEST BEGINNER A 69//
+
+            string[] s = Console.ReadLine().Split(' ');
+            int n = int.Parse(s[0]);
+            int m = int.Parse(s[1]);
+
+            Console.WriteLine((n-1)*(m-1));
+            Console.ReadKey();
+
+        }
+
+        
+    }
+}
