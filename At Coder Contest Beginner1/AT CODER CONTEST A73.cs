868d92d8d080c77e606945310fce83850ae7d563
S-SasiKumar <sasisadha815@gmail.com>
2021-09-16 17:47:46 +0530

    CONTEST A73

---
+++ At Coder Contest Beginner1/AT CODER CONTEST A73.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace september9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //AT CODER CONTEST BEGINNER A73//
+
+
+            string s = Console.ReadLine();
+  
+             
+            if(s[0]=='9'||s[1]=='9')
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        
+            Console.ReadKey();
+    }
+        
+    }
+}
