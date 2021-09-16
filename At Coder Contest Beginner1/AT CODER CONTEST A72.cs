ef51299c7fbde2b3d2b2380f61422f1a0ea6e931
S-SasiKumar <sasisadha815@gmail.com>
2021-09-16 17:45:56 +0530

    CONTEST A72

---
+++ At Coder Contest Beginner1/AT CODER CONTEST A72.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snuke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //AT CODER BEGINNER CONTEST A72//
+
+            string[] s = Console.ReadLine().Split(' ');
+            int x = int.Parse(s[0]);
+            int t = int.Parse(s[1]);
+            if(x>t)
+            {
+                Console.WriteLine(x-t);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+            Console.ReadKey();
+        }
+    }
+}
