221872f1d2d982143edb9fcca2753ee6dd9193f1
S-SasiKumar <sasisadha815@gmail.com>
2021-09-16 17:50:01 +0530

    CONTEST B72

---
+++ At Coder Contest Beginner1/AT CODER CONTEST B72.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oddstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //AT CODER BEGINNER CONTEST B72//
+
+            string s = Console.ReadLine();
+            for (int i = 0; i < s.Length;i++ )
+            {
+                if(i%2==0)
+                {
+                    Console.Write(s[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
