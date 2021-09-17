becff9471dae17c4f1637041eb568944f092a53c
S-SasiKumar <sasisadha815@gmail.com>
2021-09-17 22:05:32 +0530

    RATEING GOAL

---
+++ At Coder Contest Beginner1/AT CODER CONTEST A76.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reteing_goal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //AT CODER CONTEST BEGINNER A76//
+
+            int r = Convert.ToInt32(Console.ReadLine());
+            int g = Convert.ToInt32(Console.ReadLine());
+
+            int value = 2 * g - r;
+           
+            Console.WriteLine(value);
+            Console.ReadKey();
+        }
+    }
+}
