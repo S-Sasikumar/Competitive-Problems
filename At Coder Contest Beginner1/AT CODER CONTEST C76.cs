41923f3d1ebd0684d7d08d6143bdb153ac253eab
S-SasiKumar <sasisadha815@gmail.com>
2021-09-17 22:09:38 +0530

    ROTATION

---
+++ At Coder Contest Beginner1/AT CODER CONTEST C76.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //AT CODER BEGINNER CONTEST C76//
+
+            string s = Console.ReadLine();
+            string s1=Console.ReadLine();
+            string v = "";
+            for (int i =s.Length-1;i>=0;i-- )
+            {
+
+                v+=s[i];
+
+            }
+            if (v == s1)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+          
+            Console.ReadKey();
+        }
+    }
+}
