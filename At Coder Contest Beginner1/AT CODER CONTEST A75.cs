261c5ef6730cc3d182ee3958f0912599d9a019a2
S-SasiKumar <sasisadha815@gmail.com>
2021-09-17 21:46:09 +0530

    ONE OUT OF THREE

---
+++ At Coder Contest Beginner1/AT CODER CONTEST A75.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace one_out_of_three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AT CODER BEGINNER CONTEST A75//
+
+            string[] s = Console.ReadLine().Split();
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+            int c = int.Parse(s[2]);
+
+            if(a==c)
+            {
+                Console.WriteLine(b);
+            }
+            if(a==b)
+            {
+                Console.WriteLine(c);
+            }
+            if(b==c)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
