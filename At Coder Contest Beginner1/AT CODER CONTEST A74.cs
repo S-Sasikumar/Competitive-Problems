cef9e6f934a4b065ca78421552d4be49b7431a78
S-SasiKumar <sasisadha815@gmail.com>
2021-09-17 21:57:10 +0530

    BICHROME CELL

---
+++ At Coder Contest Beginner1/AT CODER CONTEST A74.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bichromecell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //AT CODER CONTEST BEGINNER A74//
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = Convert.ToInt32(Console.ReadLine());
+           
+
+            Console.WriteLine(n*n-a);
+
+            Console.ReadKey();
+        }
+    }
+}
