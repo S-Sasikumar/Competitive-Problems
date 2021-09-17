15feb67ff710ea974fb7708c6e513b4c7c1aa818
S-SasiKumar <sasisadha815@gmail.com>
2021-09-17 21:55:04 +0530

    ADD AND SUB

---
+++ At Coder Contest Beginner1/AT CODER CONTEST B76.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addandsub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //AT CODER BEGINNER CONTEST B 76//
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = Convert.ToInt32(Console.ReadLine());
+            int i=0;
+            int sum = 1;
+            while(i<n)
+            {
+
+                if (sum * 2 <= sum + k)
+                {
+
+                    sum += sum;
+                }
+                else
+                {
+                    sum += k;
+                }
+                i++;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
